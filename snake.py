a2e2b78acb8fea67bfea824807d2bc6f25242812
Mo Chahadeh <applednd2002@gmail.com>
2022-06-19 00:13:17 +0300

    initalized main_loop

--- snake.py
+++ snake.py
@@ -2,8 +2,29 @@ import pygame
 import numpy as np
 
 
-WIDTH = 700
-HEIGHT = 350
+WIDTH = 750
+HEIGHT = 450
+
+FPS = 60
+clock = pygame.time.Clock()
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("SnakeGameML")
+
+
+def main_loop():
+
+    running = True
+    while running:
+
+        for event in pygame.event.get():
+
+            if(event == pygame.QUIT):
+                running = False
+        
+        pygame.display.update()
+        clock.tick(FPS)
+
+
+if __name__ == "__main__":
+    main_loop()
